ce13e6a52cc92572e4875475779a135b9c501301
y894577 <44811269+y894577@users.noreply.github.com>
2020-11-27 10:34:32 +0800

    [fix]  修改部分url匹配规则

--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -3,10 +3,12 @@ from . import views
 
 urlpatterns = [
     re_path(r'^search/(?P<keyword>[\w-]+)/$', views.queryNews, name='queryNewsByCondition'),
+    re_path(r'^search/(?P<keyword>[\w-]+)/offset/(?P<page>[1-9]|[1-9]\d*)/(?P<size>[1-9]|[1-9]\d*)/$',
+            views.queryNews, name='queryNewsByKeyword'),
     re_path(r'^(?P<id>[\w-]+)/$', views.queryNews, name='queryNewsById'),
     re_path(r'^offset/(?P<page>[1-9]|[1-9]\d*)/(?P<size>[1-9]|[1-9]\d*)/$',
             views.queryNews, name='queryNewsList'),
     path('<str:id>/comment', views.queryComment, name='queryCommentById'),
-    re_path(r'^(?P<id>[\w-]+)/comment/offset/(?P<page>[1-9]|[1-9]\d*)/(?P<size>[1-9]|[1-9]\d*})/$',
+    re_path(r'^(?P<id>[\w-]+)/comment/offset/(?P<page>[1-9]|[1-9]\d*)/(?P<size>[1-9]|[1-9]\d*)/$',
             views.queryComment, name='queryCommentList'),
 ]
